8b3c0985061d37d022de11ed1ce625adf4901fd4
Ben Allen <benjaminaallenjr@gmail.com>
2023-09-16 19:11:20 -0400

    adding react router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-
+import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
 
 
 function App() {
   
 
   return (
-    <>
+    <BrowserRouter>
+      <Link>
+      </Link>
        <h1 className="text-3xl text-center mt-10 text-cyan-400">
          Hello world!
        </h1>
-
-      <h2 className="text-2xl text-center mt-9 text-red-700 text-bold"> Go Away </h2>
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+  </Routes>
+      <h2 className="text-2xl text-center mt-9 text-red-700 text-underline"> Go Away </h2>
+    </BrowserRouter>
   )
 }
 
