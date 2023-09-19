7c2819ba347c4e166d97b8025e649edcb25c3aab
Ben Allen <benjaminaallenjr@gmail.com>
2023-09-19 11:09:20 -0400

    added page compnents

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
-import {BrowserRouter,Link,Route,Routes} from 'react-router-dom'
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import {logo} from './assets'
+import {Home, CreatePost} from './page'
 
 function App() {
   
 
   return (
     <BrowserRouter>
-      <Link>
-      </Link>
-       <h1 className="text-3xl text-center mt-10 text-cyan-400">
-         Hello world!
-       </h1>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebg4]'>
+      <Link to='/'>
+        <img src={logo} className='w-28 object-contain'/>
+        </Link>
+        
+        <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
+      </header>
+       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
-        <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          
   </Routes>
-      <h2 className="text-2xl text-center mt-9 text-red-700 text-underline"> Go Away </h2>
+      </main>
     </BrowserRouter>
   )
 }
